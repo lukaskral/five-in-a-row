1c6da2a007cb3fee19ec64224da359af6a03b33a
Lukas Kral <monarcha@gmail.com>
2021-11-30 13:14:12 +0100

    Game error refactoring

--- src/game/game.rs
+++ src/game/game.rs
@@ -1,15 +1,14 @@
+#[path = "error.rs"]
+pub mod error;
 #[path = "score.rs"]
 pub mod score;
 use crate::game::score::Score;
-use std::error::Error;
 use std::fmt::Debug;
 
-pub trait Game: Clone {
+pub trait Game: Clone + Debug {
     type Move: Eq + Ord + PartialEq + Copy + Debug;
-    type Error;
 
     fn get_score(&self) -> Score;
-    fn do_move(&mut self, mv: Self::Move) -> Result<(), Self::Error>;
+    fn do_move(&mut self, mv: Self::Move) -> Result<(), error::Error<Self>>;
     fn get_possible_moves(&self, myself: bool) -> Vec<Self::Move>;
-    fn get_error(&self, source: Option<Box<dyn Error>>) -> Self::Error;
 }
